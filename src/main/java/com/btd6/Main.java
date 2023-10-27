e365e0af424ea95443ce35f085edd951ec74aa82
JanohDerOrnitorenk <erdem6505@outlook.de>
2023-10-27 12:09:11 +0200

    add database connection

--- src/main/java/com/btd6/Main.java
+++ src/main/java/com/btd6/Main.java
@@ -1,7 +1,15 @@
 package com.btd6;
 
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+        final Jdbi jdbi = Jdbi.create("jdbc:mariadb://localhost:3306/BTD6", "BTD6", "");
+        System.out.println("connecting");
+
+        final Handle handle = jdbi.open();
 }

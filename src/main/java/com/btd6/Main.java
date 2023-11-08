ad1e60026871c9f8b09dd3a84f0b597452e5694e
AntiCake <peter.istvanfi@interhyp.de>
2023-11-08 08:32:55 +0100

    fix Main.java

--- src/main/java/com/btd6/Main.java
+++ src/main/java/com/btd6/Main.java
@@ -1,15 +1,14 @@
 package com.btd6;
 
-import org.jdbi.v3.core.Handle;
 import org.jdbi.v3.core.Jdbi;
-import java.sql.DatabaseMetaData;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
         final Jdbi jdbi = Jdbi.create("jdbc:mariadb://localhost:3306/BTD6", "BTD6", "");
         System.out.println("connecting");
 
-        final Handle handle = jdbi.open();
+        jdbi.useHandle(handle -> {
+        });
+
+    }
 }

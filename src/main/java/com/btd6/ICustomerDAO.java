a12ae7df4d7ea33a39a41c72b1fd2627e28b1576
AntiCake <peter.istvanfi@interhyp.de>
2023-11-08 14:37:07 +0100

    create and remove table for customers and readings

--- src/main/java/com/btd6/ICustomerDAO.java
+++ src/main/java/com/btd6/ICustomerDAO.java
@@ -1,4 +1,18 @@
 package com.btd6;
-public interface ICustomerDAO extends IDAO<ICustomer>{
 
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+public interface ICustomerDAO extends IDAO<ICustomer>{
+    @Override
+    @SqlUpdate("CREATE TABLE customers (" +
+            "UUID VARCHAR(255) NOT NULL," +
+            "Firstname VARCHAR(255)," +
+            "Lastname VARCHAR(255)," +
+            "PRIMARY KEY (UUID)" +
+            ")")
+    void createTable();
+
+    @Override
+    @SqlUpdate("DROP TABLE customers;")
+    void removeTable();
 }
--- src/main/java/com/btd6/IReadingDAO.java
+++ src/main/java/com/btd6/IReadingDAO.java
@@ -1,5 +1,24 @@
 package com.btd6;
 
-public interface IReadingDAO extends IDAO<IReading>{
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
+public interface IReadingDAO extends IDAO<IReading>{
+    @Override
+    @SqlUpdate("CREATE TABLE readings (" +
+            "UUID VARCHAR(255) NOT NULL," +
+            "Comment VARCHAR(255)," +
+            "Customer VARCHAR(255)," +
+            "DateOfReading DATE," +
+            "Metercount DOUBLE," +
+            "MeterId VARCHAR(255)," +
+            "Substitute TINYINT(1)," +
+            "PRIMARY KEY (UUID)," +
+            "CONSTRAINT fk_reading_customer FOREIGN KEY (Customer)" +
+            "REFERENCES customers (UUID) ON DELETE SET NULL" +
+            ")")
+    void createTable();
+
+    @Override
+    @SqlUpdate("DROP TABLE readings;")
+    void removeTable();
 }

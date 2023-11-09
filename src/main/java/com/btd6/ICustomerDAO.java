22e9f34422796f60960d451193671298cbbea593
AntiCake <peter.istvanfi@interhyp.de>
2023-11-09 08:16:37 +0100

    implement customers dao

--- src/main/java/com/btd6/ICustomerDAO.java
+++ src/main/java/com/btd6/ICustomerDAO.java
@@ -1,6 +1,10 @@
 package com.btd6;
 
+import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface ICustomerDAO extends IDAO<ICustomer>{
     @Override
@@ -13,6 +17,30 @@ public interface ICustomerDAO extends IDAO<ICustomer>{
     void createTable();
 
     @Override
-    @SqlUpdate("DROP TABLE customers;")
+    @SqlUpdate("DROP TABLE customers")
     void removeTable();
+
+    @Override
+    @SqlUpdate("INSERT INTO customers (UUID, Firstname, Lastname) VALUES (:o.getUuid(), :o.getFirstname(), :o.getLastname())")
+    boolean insert(ICustomer o);
+
+    @Override
+    @SqlQuery("SELECT * FROM customers WHERE UUID = :id")
+    ICustomer findById(UUID id);
+
+    @Override
+    @SqlQuery("SELECT * FROM customers")
+    List<? extends ICustomer> getAll();
+
+    @Override
+    @SqlUpdate("UPDATE customers SET Firstname = :o.getFirstname(), Lastname = :o.getLastname() WHERE UUID = :o.getUuid()")
+    boolean update(ICustomer o);
+
+    @Override
+    @SqlUpdate("DELETE FROM customers WHERE UUID = :uuid")
+    boolean delete(UUID uuid);
+
+    @Override
+    @SqlUpdate("TRUNCATE TABLE customers")
+    void truncate();
 }

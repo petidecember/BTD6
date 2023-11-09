92da368c2a1db8b3e1d8bf6cd09e9ce2f5ae2347
AntiCake <peter.istvanfi@interhyp.de>
2023-11-09 08:26:39 +0100

    implement customer class

--- src/main/java/com/btd6/Customer.java
+++ src/main/java/com/btd6/Customer.java
@@ -1,5 +1,45 @@
 package com.btd6;
 
-public class Customer {
+import java.util.UUID;
 
+public class Customer implements ICustomer {
+    private UUID uuid;
+    private String firstname;
+    private String lastname;
+
+    public Customer(String firstname, String lastname) {
+        setUuid(UUID.randomUUID());
+        setFirstname(firstname);
+        setLastname(lastname);
+    }
+
+    @Override
+    public String getFirstname() {
+        return firstname;
+    }
+
+    @Override
+    public void setFirstname(String firstName) {
+        this.firstname = firstname;
+    }
+
+    @Override
+    public UUID getUuid() {
+        return this.uuid;
+    }
+
+    @Override
+    public void setUuid(UUID id) {
+        this.uuid = id;
+    }
+
+    @Override
+    public String getLastname() {
+        return this.lastname;
+    }
+
+    @Override
+    public void setLastname(String lastName) {
+        this.lastname = lastName;
+    }
 }

55ec662fc1a870ce7aacae38e02e96770ce00ea5
Coldstor <93476610+Coldstor@users.noreply.github.com>
2024-01-10 08:29:38 +0100

    solved some Problems

---
+++ src/test/java/com/btd6/ReadingFactory.java
@@ -0,0 +1,25 @@
+package com.btd6;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ReadingFactory {
+
+    private final Connection connection;
+
+    public ReadingFactory(Connection connection) {
+        this.connection = connection;
+    }
+
+    public void saveReading(Reading reading) throws SQLException {
+        String sql = "INSERT INTO readings (first_name, last_name) VALUES (?, ?)";
+
+        try (PreparedStatement preparedStatement = connection.prepareStatement(sql)) {
+            preparedStatement.setString(1, reading.getMeterId());
+            preparedStatement.setString(2, reading.getComment());
+            preparedStatement.executeUpdate();
+        }
+    }
+}
+
